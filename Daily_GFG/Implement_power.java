05d81bf1e3a6c56cbef513c1dd82f771ee7488f0
Monikamali0409 <monikamali8263@gmail.com>
2025-01-29 10:52:23 +0530

    Implement power of given number

---
+++ Daily_GFG/Implement_power.java
@@ -0,0 +1,29 @@
+package Daily_GFG;
+
+public class Implement_power {
+    double power(double b, int e) {
+        // code here
+        
+        int m = e;
+        double ans = 1.0;
+        
+        if(e < 0) e = -1 * e;
+        
+        while(e > 0){
+            if(e % 2 == 1){
+                ans = ans * b;
+                e = e - 1;
+            }
+            else{
+                e = e / 2;
+                b = b * b;
+            }
+        }
+        
+        if(m < 0){
+           ans = (double)1.0 / (double)ans; 
+        } 
+        
+        return ans;
+    }
+}
