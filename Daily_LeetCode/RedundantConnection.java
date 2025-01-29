5448342ec86df27a6fc5ee2fc6420efb7e4eda96
Monikamali0409 <monikamali8263@gmail.com>
2025-01-29 11:32:46 +0530

    Remove redundant connection that form cycle

---
+++ Daily_LeetCode/RedundantConnection.java
@@ -0,0 +1,33 @@
+package Daily_LeetCode;
+
+import java.util.Arrays;
+
+public class RedundantConnection {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        int[] dsuf = new int[n+1];
+        Arrays.fill(dsuf, -1);
+
+        for(int[] edge : edges){
+            int parentX = find(dsuf, edge[0]);
+            int parentY = find(dsuf, edge[1]);
+
+            if(parentX == parentY){
+                return edge;
+            }
+            else{
+                dsuf[parentX] = parentY;
+            }
+        }
+
+        return new int[]{0,0};
+    }
+
+    private int find(int[] dsuf, int v){
+        if(dsuf[v] == -1){
+            return v;
+        }
+
+        return dsuf[v] = find(dsuf, dsuf[v]);
+    }
+}
