96bdfeb4a08d90377c88c1953025704a2fc74818
Monikamali0409 <monikamali8263@gmail.com>
2025-03-04 11:08:33 +0530

    Check if Number is a Sum of Powers of Three

---
+++ Daily_LeetCode/power3.java
@@ -0,0 +1,15 @@
+package Daily_LeetCode;
+
+public class power3 {
+    public boolean checkPowersOfThree(int n) {
+        while(n > 0){
+            if(n % 3 == 2){
+                return false;
+            }
+
+            n = n / 3;
+        }
+
+        return true;
+    }
+}
