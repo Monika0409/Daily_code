e75afa3ab210984be0f481ac39b47367bc071680
Monikamali0409 <monikamali8263@gmail.com>
2025-03-05 11:55:04 +0530

    Longest String Chain

---
+++ Daily_GFG/StringChain.java
@@ -0,0 +1,26 @@
+package Daily_GFG;
+
+public class StringChain {
+    public int longestStringChain(String words[]) {
+        // code here
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        Map<String , Integer> map = new HashMap<>();
+        int maxLen = 1;
+        
+        for(String word : words){
+            int maxChain = 1;
+            for(int i=0; i<word.length(); i++){
+                String predecessor = word.substring(0, i) + word.substring(i+1);
+                
+                if(map.containsKey(predecessor)){
+                    maxChain = Math.max(maxChain, map.get(predecessor) + 1);
+                }
+            } 
+            
+            map.put(word, maxChain);
+            
+            maxLen = Math.max(maxLen, maxChain);
+        }
+        return maxLen;
+    }
+}
